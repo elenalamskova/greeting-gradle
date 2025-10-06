73f053edc542d2bbaa7dd09b6efb8d2f6e666a99
ElenaLamskova <elena.katysheva23062000@gmail.com>
2025-10-06 21:11:30 +0500

    Configure Gradle build and add initial tests

---
+++ src/test/java/ru/mentee/power/GreetingTest.java
@@ -0,0 +1,14 @@
+package ru.mentee.power;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GreetingTest {
+    @Test
+    void shouldGreeting() {
+        Greeting greeting = new Greeting();
+        String result = greeting.greeting();
+        assertEquals(result, "Hello MP-10 done");
+    }
+}
